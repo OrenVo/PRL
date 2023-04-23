c6c8c296d303e50244c47767fa3e0132a043fbfa
Vojtěch Ulej <xulejv00@stud.fit.vutbr.cz>
2023-04-23 16:50:05 +0200

    coding completed. Starting documentation

--- 4-means/parkmeans.cc
+++ 4-means/parkmeans.cc
@@ -6,80 +6,116 @@
 /***************************/
 
 
+#include<cstddef>
 #include<iostream>
 #include<fstream>
-#include<mpi.h>
 #include<cstdint>
 #include<sstream>
+#include <string>
 #include<vector>
 #include<array>
+
+#include<mpi.h>
 
 #define ifname "numbers"
 #define MAX_ITERATIONS 1000
 #define THRESHOLD 1e-6
 
 namespace prl {
+    // Logging
+    void log(std::stringstream &message){
+      std::cerr << message.str() << std::endl;
+    }
+    void log(std::string &message){
+      std::cerr << message << std::endl;
+    }
+    void log(std::string message){
+        std::cerr << message << std::endl;
+      }
+
     // Load numbers from binary file
     std::vector<uint8_t> loadFile(std::string filename=ifname){
         std::vector<uint8_t> numbers;
         std::ifstream infile(filename, std::ios::binary);
-            if (infile) {
-                while (!infile.eof()) {
-                    uint8_t number;
-                    infile.read((char*)&number, sizeof(number));
-                    numbers.push_back(number);
-                }
-                infile.close();
-                return numbers;
-            } else {
-                infile.close();
-                std::cerr << "Error opening file: " << filename << std::endl;
-                MPI_Abort(MPI_COMM_WORLD, 1);
-                return numbers;
-            }
-    }
+        if (infile) {
+          // Get the file size
+          infile.seekg(0, std::ios::end);
+          size_t file_size = infile.tellg();
+          infile.seekg(0, std::ios::beg);
 
-    
-    
+          // Read the contents of the file into a buffer
+          std::vector<uint8_t> numbers(file_size);
+          infile.read(reinterpret_cast<char*>(numbers.data()), file_size);
+
+          infile.close();
+          return numbers;
+        } else {
+          infile.close();
+          std::cerr << "Error opening file: " << filename << std::endl;
+          MPI_Abort(MPI_COMM_WORLD, 1);
+          return numbers;
+        }
+    }
+    // Calculate distance of value from mean of cluster
     inline float distance(uint8_t value, float mean){
         return std::abs(float(value) - mean);
     }
     
-    enum Cluster {
+    enum Cluster : uint8_t {
         A=0, B, C, D
     };
 
-    void updateClusters(std::array<float,4> &medians, uint8_t val, prl::Cluster cluster){
-        //TODO MPI_AllReduce or MPI_Reduce to calculate new center for each cluster and store it in medians.
-        //
+    void updateClusters(std::array<float,4> &medians, uint8_t val, prl::Cluster cluster, int rank){
+      // Calculate counts of elements in each clusters
+      size_t sizeA, sizeB, sizeC, sizeD;
+      size_t clusA = cluster == Cluster::A ? 1 : 0;
+      size_t clusB = cluster == Cluster::B ? 1 : 0;
+      size_t clusC = cluster == Cluster::C ? 1 : 0;
+      size_t clusD = cluster == Cluster::D ? 1 : 0;
+      MPI_Allreduce(&clusA, &sizeA, 1, MPI_UINT64_T, MPI_SUM, MPI_COMM_WORLD);
+      MPI_Allreduce(&clusB, &sizeB, 1, MPI_UINT64_T, MPI_SUM, MPI_COMM_WORLD);
+      MPI_Allreduce(&clusC, &sizeC, 1, MPI_UINT64_T, MPI_SUM, MPI_COMM_WORLD);
+      MPI_Allreduce(&clusD, &sizeD, 1, MPI_UINT64_T, MPI_SUM, MPI_COMM_WORLD);
+
+      // Calculate average for each cluster and use it as mean in next iteration.
+      uint64_t sumA, sumB, sumC, sumD;
+      uint64_t valA = cluster == Cluster::A ? uint64_t(val) : 0;
+      uint64_t valB = cluster == Cluster::B ? uint64_t(val) : 0;
+      uint64_t valC = cluster == Cluster::C ? uint64_t(val) : 0;
+      uint64_t valD = cluster == Cluster::D ? uint64_t(val) : 0;
+      // Calculate sums of each cluster
+      MPI_Allreduce(&valA, &sumA, 1, MPI_UINT64_T, MPI_SUM, MPI_COMM_WORLD);
+      MPI_Allreduce(&valB, &sumB, 1, MPI_UINT64_T, MPI_SUM, MPI_COMM_WORLD);
+      MPI_Allreduce(&valC, &sumC, 1, MPI_UINT64_T, MPI_SUM, MPI_COMM_WORLD);
+      MPI_Allreduce(&valD, &sumD, 1, MPI_UINT64_T, MPI_SUM, MPI_COMM_WORLD);
+
+      std::array<size_t, 4> sizes = {sizeA, sizeB, sizeC, sizeD};
+      std::array<uint64_t , 4> sums = {sumA, sumB, sumC, sumD};
+      for (size_t i = 0; i < 4; i++){
+        size_t size = sizes[i];
+        uint64_t sum = sums[i];
+        if (size > 0)
+            medians[i] = float(sum)/float(size);
+      }
+      //log("Process: " + std::to_string(rank) + ": got averages: [ " + std::to_string(medians[0]) + ", " + std::to_string(medians[1]) + ", " + std::to_string(medians[2]) + ", " + std::to_string(medians[3]) + " ]" );
     }
     
     Cluster assignCluster(float dis1, float dis2, float dis3, float dis4){
-        float min = dis1;
-        Cluster cluster = Cluster::A;
-        if (dis2 < min) {
-            min = dis2;
-            cluster = Cluster::B;
-        }
-        if (dis3 < min) {
-            min = dis3;
-          cluster = Cluster::C;
-        }
-        if (dis4 < min) {
-            min = dis4;
-            cluster = Cluster::D;
-        }
-        return cluster;
-    }
-
-    void log(std::stringstream &message){
-        std::cerr << message.str() << std::endl;
-    }
-    void log(std::string &message){
-        std::cerr << message << std::endl;
-    }
-    void log(std::string message){
-        std::cerr << message << std::endl;
+      float min = dis1;
+      Cluster cluster = Cluster::A;
+      if (dis2 < min) {
+          min = dis2;
+          cluster = Cluster::B;
+      }
+      if (dis3 < min) {
+          min = dis3;
+        cluster = Cluster::C;
+      }
+      if (dis4 < min) {
+          min = dis4;
+          cluster = Cluster::D;
+      }
+      return cluster;
     }
 }
 
@@ -111,30 +147,58 @@ int main(int argc, char *argv[]) {
         ss << medians[prl::Cluster::B] << ", ";
         ss << medians[prl::Cluster::C] << ", ";
         ss << medians[prl::Cluster::D] << "]" << std::endl;
-        prl::log("Medians: " + ss.str());
     }
     // broadcast medians to all processes
     MPI_Bcast(medians.data(), 4, MPI_FLOAT, root, MPI_COMM_WORLD);
     // scatter numbers to processes (each process gets one number)
     uint8_t number;
+    prl::Cluster cluster;
     MPI_Scatter(numbers.data(), 1, MPI_UINT8_T, &number, 1, MPI_UINT8_T, root, MPI_COMM_WORLD);
     for (size_t i = 0; i < MAX_ITERATIONS; i++){
         // calculate distance from each number to each median
-        prl::Cluster cluster = prl::assignCluster(
+        cluster = prl::assignCluster(
             prl::distance(number, medians[prl::Cluster::A]),
             prl::distance(number, medians[prl::Cluster::B]),
             prl::distance(number, medians[prl::Cluster::C]),
             prl::distance(number, medians[prl::Cluster::D])
             );
-        prl::log("Process " + std::to_string(rank) + " got number: " + std::to_string(number) + " and add it to cluster: " + std::to_string(cluster) + " median: " + std::to_string(medians[cluster]));
         //update clusters for all nodes and check if end conditions are met
-        prl::updateClusters(medians, number, cluster);
-        // TODO what next?
+        std::array<float, 4> oldMedians = medians;
+        prl::updateClusters(medians, number, cluster, rank);
+        if (medians == oldMedians){ // no medians were changed
+          break;
+        }
     }
-    
+
+    uint8_t *clusters;
+    if (rank == root){
+      clusters = new uint8_t[numbers.size()];
+      for (int i = 0; i < numbers.size(); i++){
+        clusters[i] = 0;
+      }
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Gather(&cluster, 1, MPI_UINT8_T, clusters, 1, MPI_UINT8_T, root, MPI_COMM_WORLD);
+    if (rank == root){
+      std::array<std::vector<uint8_t>,4> clustersValues;
+
+      for (size_t i = 0; i < numbers.size(); i++){
+        clustersValues[clusters[i]].push_back(numbers[i]);
+      }
+      std::stringstream ss;
+      for (size_t i = 0; i < 4; i++){
+        ss << "[" << std::to_string(medians[i]) << "]";
+        for (auto val : clustersValues[i]) {
+          ss << " " << std::to_string(val) << ",";
+        }
+        ss.seekp(-1, std::ios_base::end);
+        ss << std::endl;
+      }
+      std::cout << ss.str();
+    }
     // clean up because OpenMPI cannot work with C++ STL containers
     if (rank == root) {
-        
+      delete[] clusters;
     }
     
     MPI_Finalize();
--- 4-means/test.sh
+++ 4-means/test.sh
@@ -8,7 +8,7 @@ else
 fi;
 
 #preklad cpp zdrojaku
-mpic++ --prefix /usr/local/share/OpenMPI -o parkmeans parkmeans.cc
+mpic++ --prefix /usr/local/share/OpenMPI -g -o parkmeans parkmeans.cc
 
 
 #vyrobeni souboru s random cisly
@@ -17,7 +17,9 @@ dd if=/dev/random bs=1 count=$numbers of=numbers
 #spusteni
 mpirun --prefix /usr/local/share/OpenMPI --oversubscribe -np $numbers parkmeans
 
-#mpirun --prefix /usr/local/share/OpenMPI -np $processes valgrind --suppressions=/usr/local/share/OpenMPI/share/openmpi/openmpi-valgrind.supp ./parsplit
+#mpirun --prefix /usr/local/share/OpenMPI -np $numbers valgrind --suppressions=/usr/local/share/OpenMPI/share/openmpi/openmpi-valgrind.supp ./parkmeans
+#mpirun --prefix /usr/local/share/OpenMPI -np $numbers valgrind --suppressions=/usr/lib64/mpi/gcc/openmpi4/share/openmpi/openmpi-valgrind.supp  ./parkmeans
+
 
 #uklid
 # rm -f oems numbers
